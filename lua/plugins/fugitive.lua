39046c007a4b3f66839250e86a40fcf018aff519
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2023-08-30 22:11:09 +0200

    add keymaps and cmd to fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,8 @@
 return {
   "tpope/vim-fugitive",
+  cmd = {"G", "Git"},
+  keys = {
+    {"<leader>ga", ":Git fetch --all -p<cr>", desc = "Git fetch"},
+    {"<leader>gl", ":Git pull<cr>", desc = "Git pull"},
+  },
 }
