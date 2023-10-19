491b0a9408422d15b8a750204edf2d1f777e6afb
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2023-10-19 21:28:31 +0200

    telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -33,17 +33,48 @@ return {
     },
     {
       "<leader>pe",
-      function ()
+      function()
         require("telescope.builtin").buffers()
       end,
       desc = "Telescope buffers",
     },
     {
+      "<leader>gs",
+      function()
+        require("telescope.builtin").git_status()
+      end,
+      desc = "Telescope Git status",
+    },
+    {
+      "<leader>gc",
+      function()
+        require("telescope.builtin").git_bcommits()
+      end,
+      desc = "Telescope Git status",
+    },
+    {
+      "<leader>gb",
+      function()
+        require("telescope.builtin").git_branches()
+      end,
+      desc = "Telescope Git branches",
+    },
+    {
       "<leader>rp",
-      function ()
+      function()
         require("telescope.builtin").find_files({
           prompt_title = "Plugins",
           cwd = "~/.config/nvim/lua/plugins",
+          attach_mappings = function(_, map)
+            local actions = require("telescope.actions")
+            local action_state = require("telescope.actions.state")
+            map("i", "<c-y>", function(prompt_bufnr)
+              local new_plugin = action_state.get_current_line()
+              actions.close(prompt_bufnr)
+              vim.cmd(string.format("edit ~/.config/nvim/lua/plugins/%s.lua", new_plugin))
+            end)
+            return true
+          end
         })
       end
     },
@@ -63,8 +94,8 @@ return {
     },
     {
       "<leader>bb",
-      function ()
-        require("telescope").extensions.file_browser.file_browser({path = "%:h:p", select_buffer = true})
+      function()
+        require("telescope").extensions.file_browser.file_browser({ path = "%:h:p", select_buffer = true })
       end,
       desc = "Telescope file browser"
     }
