78f21514a20163ffee224d3031ff98b14d06a5cf
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2023-12-02 13:52:54 +0100

    feat: add lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    options = {
+      globalstatus = true,
+    },
+    sections = {
+      lualine_c = { { "filename", file_status = true, path = 1, } },
+      lualine_x = {
+        {
+          require("lazy.status").updates,
+          cond = require("lazy.status").has_updates,
+          color = { fg = "#ff9e64" },
+        },
+      },
+    },
+    inactive_winbar = {
+      lualine_c = { "filename" },
+    },
+  },
+}
